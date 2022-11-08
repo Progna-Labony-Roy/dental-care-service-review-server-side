a1a51d30f7b32e5db6b292f1685ada2e629a227e
Progna_Labony_Roy <labony.progna@gmail.com>
2022-11-09 00:25:42 +0530

    request for a dynamic route code added

--- index.js
+++ index.js
@@ -3,12 +3,40 @@ const cors = require('cors');
 const app= express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //  https://y-red-phi.vercel.app
 
-
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.goxtca6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollections= client.db('service-review-db').collection('serviecs');
+
+        app.get('/services',async(req,res) =>{
+            const query ={};
+            const cursor = serviceCollections.find(query);
+            const services= await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/services/:id', async(req,res) =>{
+            id=req.params.id;
+            const query ={_id: ObjectId(id)};
+            const service= await serviceCollections.findOne(query);
+            res.send(service)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch( err =>console.err(err));
 
 
 app.get('/',(req , res)=>{
